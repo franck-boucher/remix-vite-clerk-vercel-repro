1141e10d99d543fdecf143d76fc37bc2a3322be9
Franck Boucher <boucher.fk@gmail.com>
2024-03-16 19:18:00 +0100

    add clerk

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@vercel/remix";
 
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/remix";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -9,33 +11,40 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <SignedIn>
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+          <h1>Welcome to Remix</h1>
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                href="https://remix.run/tutorials/blog"
+                rel="noreferrer"
+              >
+                15m Quickstart Blog Tutorial
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://remix.run/tutorials/jokes"
+                rel="noreferrer"
+              >
+                Deep Dive Jokes App Tutorial
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+                Remix Docs
+              </a>
+            </li>
+          </ul>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </div>
   );
 }
